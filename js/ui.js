c3d1713d9ff0d6b9cb30bc5cae40cbee858b1bb1
elinahedenius <bohedenius@protonmail.com>
2020-02-28 17:56:00 +0100

    added a floating action button

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,6 @@
 
+var fabToggled = false;
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
@@ -14,6 +16,7 @@ function reflectCurrentView(){
 var view = document.getElementsByClassName('view');
 var viewBtn = document.getElementsByClassName('viewBtn');
 
+//Changes view to the one clicked in the navbar
 function changeView(e){
   var name = e.getAttribute('id');
   var toOpen = name + 'View';
@@ -24,12 +27,38 @@ function changeView(e){
   document.getElementById(toOpen).style.display = 'grid';
 }
 
-function hello(){
-  alert('hello');
-}
-
+//litens for clicks on the navbar buttons
 document.addEventListener('click', function(evt){
-  if (evt.target.className === 'btn-flat'){
+  if (evt.target.className === 'viewBtn'){
     changeView(evt.target);
   }
 }, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: false
+  });
+});
+
+//Action button
+//function toggleAddOptions(){
+//  var addOptions = document.getElementById('addOptions');
+//  if (addOptionsLoaded){
+//    addOptions.style.display ='none';
+//    document.getElementById('fab').style.transform = 'rotate(0deg)';
+//    addOptionsLoaded = false;
+//  }
+//  else {
+//    addOptions.style.display = 'flex';
+//    document.getElementById('fab').style.transform = 'rotate(45deg)';
+//    addOptionsLoaded = true;
+//  }
+//}
